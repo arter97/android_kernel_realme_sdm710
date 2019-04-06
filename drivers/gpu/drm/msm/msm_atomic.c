a8eb7340a2cdbaaae816f6bb310140bb7de0520d
Park Ju Hyung <qkrwngud825@gmail.com>
2019-04-07 01:39:39 +0900

    drm/msm: do not notify events when the system is shutting down
    It's been observed that a panic could occur due to a race with
    a MSM_DRM_BLANK_POWERDOWN handler and a driver removal function
    upon shutdown while the screen is on.
    
    Since this shouldn't happen, send DRM events only when the system is running.
    
    This doesn't seem to be easy to detect as the events are sent after the
    userspace drm process terminates, which is before init binary notifies
    the kernel about the shutdown.
    
    Workaround this by detecting shutdown upon sysrq kill-all-tasks(i),
    sent from the init binary.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/gpu/drm/msm/msm_atomic.c
+++ drivers/gpu/drm/msm/msm_atomic.c
@@ -72,21 +72,22 @@ EXPORT_SYMBOL(msm_drm_unregister_client);
  * @v: notifier data, inculde display id and display blank
  *     event(unblank or power down).
  */
-#ifndef VENDOR_EDIT
-/*liping-m@PSW.MM.Display.Lcd.Stability, 2018/9/26,add for  export drm_notifier*/
-static int msm_drm_notifier_call_chain(unsigned long val, void *v)
-{
-	return blocking_notifier_call_chain(&msm_drm_notifier_list, val,
-					    v);
-}
-#else /*VENDOR_EDIT*/
+static bool notifier_enabled __read_mostly = true;
 int msm_drm_notifier_call_chain(unsigned long val, void *v)
 {
+	if (unlikely(!notifier_enabled))
+		return 0;
+
 	return blocking_notifier_call_chain(&msm_drm_notifier_list, val,
 					    v);
 }
-EXPORT_SYMBOL(msm_drm_notifier_call_chain);
-#endif /*VENDOR_EDIT*/
+
+void msm_drm_notifier_enable(bool val)
+{
+	notifier_enabled = val;
+	mb();
+}
+EXPORT_SYMBOL(msm_drm_notifier_enable);
 
 /* block until specified crtcs are no longer pending update, and
  * atomically mark them as pending update
